070fc3ab67ca6aea34ac82aa9c377f41a374cac4
Daniel Standage <daniel.standage@gmail.com>
2017-05-27 10:04:32 -0700

    Tests for main method

--- nuclmm/tests/test_package.py
+++ nuclmm/tests/test_package.py
@@ -7,7 +7,9 @@
 
 from __future__ import print_function
 import pytest
+import subprocess
 import nuclmm
+from nuclmm import data_file
 
 
 def test_open_badmode():
@@ -21,3 +23,17 @@ def test_fasta():
     infile = nuclmm.open(infilename, 'r')
     seqs = [seq for defline, seq in nuclmm.parse_fasta(infile)]
     assert len(seqs) == 3
+
+
+def test_main(capfd):
+    traincmd = './cli train --order 4 {}'.format(data_file('aaat.fa'))
+    trainargs = traincmd.split()
+    trainproc = subprocess.Popen(trainargs, stdout=subprocess.PIPE)
+
+    simcmd = './cli simulate --order 4 --numseqs 1 --seqlen 32 --seed 42 -'
+    simargs = simcmd.split()
+    simproc = subprocess.Popen(simargs, stdin=trainproc.stdout)
+
+    simproc.communicate()
+    out, err = capfd.readouterr()
+    assert 'AAATAAATAAATAAATAAATAAATAAATAAAT' in out
